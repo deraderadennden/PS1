22e190558b28d0395e2648e4fad73bb09ce245c5
deraderadenndenn <48232598+deraderadenndenn@users.noreply.github.com>
2019-03-07 23:58:47 +0900

    Initial commit

---
+++ KerBoardSetting.ps1
@@ -0,0 +1,9 @@
+﻿
+#
+$regpath = "HKLM:\SYSTEM\CurrentControlSet\Services\i8042prt\Parameters"
+
+#
+Set-ItemProperty $regpath -Name "LayerDriver JPN" -Value "kbd106.dll"
+Set-ItemProperty $regpath -Name "OverrideKeyboardIdentifier" -Value "PCAT_106KEY"
+Set-ItemProperty $regpath -Name "OverrideKeyboardSubtype" -Value "2"
+Set-ItemProperty $regpath -Name "OverrideKeyboardType" -Value "7"
---
+++ SetJapanese.ps1
@@ -0,0 +1,35 @@
+﻿#01
+
+$dir_path   = "C:\Windows\Temp\"
+$dir_dest   = $dir_path + $url_dlfile
+
+$url_ms     = "http://download.windowsupdate.com/d/msdownload/update/software/updt/"
+$url_yyyymm = "2018/04/"
+$url_dlfile = "lp_bac048dbf02b9166cfc0fd1a926c241e64955d6f.cab"
+
+$url_source = $url_ms + $url_yyyymm + $url_dlfile
+
+#
+Set-WinHomeLocation 122
+
+#
+Set-WinUserLanguageList ja-JP,en-US -Force
+
+#
+Start-BitsTransfer -Source $url_source -Destination $dir_path -Priority High
+
+#
+Add-WindowsPackage -Online -PackagePath $dir_dest
+
+#
+Set-WinLanguageBarOption -UseLegacySwitchMode -UseLegacyLanguageBar
+
+#
+Set-WinDefaultInputMethodOverride 
+
+#
+Set-WinUILanguageOverride -Language ja-JP
+
+
+
+
